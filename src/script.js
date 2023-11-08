b8a7ef721faf1c1abc895fdcacd19c792be10805
048Lucasoliveira <148930690+048Lucasoliveira@users.noreply.github.com>
2023-11-07 23:26:46 -0300

    Update Photos

--- src/script.js
+++ src/script.js
@@ -37,6 +37,34 @@ function login() {
       document.write("<center><img src='img/3.jpg' class='herry' id=herry' width=400 'align='middle'></center>")  
     
     }
+    else if(pesquisarimg == 4) {
+      document.write("<center><img src='img/4.jpg' class='herry' id=herry' width=400 'align='middle'></center>")  
+    
+    }
+    else if(pesquisarimg == 5) {
+      document.write("<center><img src='img/5.jpg' class='herry' id=herry' width=400 'align='middle'></center>")  
+    
+    }
+    else if(pesquisarimg == 6) {
+      document.write("<center><img src='img/6.jpg' class='herry' id=herry' width=400 'align='middle'></center>")  
+    
+    }
+    else if(pesquisarimg == 7) {
+      document.write("<center><img src='img/7.jpg' class='herry' id=herry' width=400 'align='middle'></center>")  
+    
+    }
+    else if(pesquisarimg == 8) {
+      document.write("<center><img src='img/8.jpg' class='herry' id=herry' width=400 'align='middle'></center>")  
+    
+    }
+    else if(pesquisarimg == 9) {
+      document.write("<center><img src='img/9.jpg' class='herry' id=herry' width=400 'align='middle'></center>")  
+    
+    }
+    else if(pesquisarimg == 10) {
+      document.write("<center><img src='img/10.jpg' class='herry' id=herry' width=400 'align='middle'></center>")  
+    
+    }
     else {
         console.log('Digite um numero de 1 a 100')
         alert("Digite um numero de 1 a 100")
